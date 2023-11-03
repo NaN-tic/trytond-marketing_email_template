42daecf2d05d6fa95d69cd1072a2254a177966f2
Albert Cervera i Areny <albert@nan-tic.com>
2023-11-03 21:12:12 +0100

    Add default values for from test_mail and template.

--- __init__.py
+++ __init__.py
@@ -6,5 +6,9 @@ from . import marketing
 
 def register():
     Pool.register(
+        marketing.EmailList,
         marketing.Message,
         module='marketing_email_template', type_='model')
+    Pool.register(
+        marketing.SendTest,
+        module='marketing_email_template', type_='wizard')
